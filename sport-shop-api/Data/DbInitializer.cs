aa326cc1642679912d7b252949fd27ef92adec0c
vtvy <votrieuvy1661@gmail.com>
2023-04-15 11:58:07 +0700

    change format db init

--- sport-shop-api/Data/DbInitializer.cs
+++ sport-shop-api/Data/DbInitializer.cs
@@ -14,8 +14,8 @@ namespace sport_shop_api.Data
             {
                 List<User> users = new()
                 {
-                    new User() { Email = configuration["Accounts:admin-email"], Password = BC.HashPassword(configuration["Accounts:admin-pass"]), Role = "Admin", Name = "Admin", Address = "CIT" },
-                    new User() { Email = configuration["Accounts:vy-email"], Password = BC.HashPassword(configuration["Accounts:vy-pass"]), Role = "Admin", Name = "Trieu Vy", Address = "OFFICE"},
+                    new User() { UserId=1, Email = configuration["Accounts:admin-email"], Password = BC.HashPassword(configuration["Accounts:admin-pass"]), Role = "Admin", Name = "Admin", Address = "CIT" },
+                    new User() { UserId=2, Email = configuration["Accounts:vy-email"], Password = BC.HashPassword(configuration["Accounts:vy-pass"]), Role = "Admin", Name = "Trieu Vy", Address = "OFFICE"},
                 };
                 context.Users.AddRange(users);
                 context.SaveChanges();
@@ -27,8 +27,8 @@ namespace sport_shop_api.Data
             {
                 List<Category> categories = new()
             {
-                new Category() { Name = "Sock" },
-                new Category() { Name = "Shirt" },
+                new Category() { CategoryId=1, Name = "Sock" },
+                new Category() { CategoryId=2, Name = "Shirt" },
             };
                 context.Categories.AddRange(categories);
                 context.SaveChanges();
@@ -39,8 +39,8 @@ namespace sport_shop_api.Data
             {
                 List<Product> products = new()
             {
-                new Product() { Name = "Cose", Quantity = 2000, Url = "https://www.rocconsulting.com/wp-content/uploads/2017/01/JDTrafford_1772.jpg", Price = 100000, Description = "This is very beautiful sock", CategoryId = 1 },
-                new Product() { Name = "Lenvo", Quantity = 2000, Url = "https://www.rocconsulting.com/wp-content/uploads/2017/01/JDTrafford_1772.jpg", Price = 200000, Description = "This is very much pretty for boy", CategoryId = 2},
+                new Product() { Id=1, Name = "Cose", Quantity = 2000, Url = "https://www.rocconsulting.com/wp-content/uploads/2017/01/JDTrafford_1772.jpg", Price = 100000, Description = "This is very beautiful sock", CategoryId = 1 },
+                new Product() { Id=2, Name = "Lenvo", Quantity = 2000, Url = "https://www.rocconsulting.com/wp-content/uploads/2017/01/JDTrafford_1772.jpg", Price = 200000, Description = "This is very much pretty for boy", CategoryId = 2},
             };
                 context.Products.AddRange(products);
                 context.SaveChanges();
@@ -51,12 +51,12 @@ namespace sport_shop_api.Data
             {
                 List<ProductSize> productSizes = new()
             {
-                new ProductSize() { Name = "S", Price = 100000, ProductId = 1 },
-                new ProductSize() { Name = "L", Price = 100000, ProductId = 1 },
-                new ProductSize() { Name = "M", Price = 100000, ProductId = 1 },
-                new ProductSize() { Name = "S", Price = 200000, ProductId = 2 },
-                new ProductSize() { Name = "L", Price = 200000, ProductId = 2 },
-                new ProductSize() { Name = "M", Price = 200000, ProductId = 2 },
+                new ProductSize() { Id=1, Name = "S", Price = 100000, ProductId = 1 },
+                new ProductSize() { Id=2, Name = "L", Price = 100000, ProductId = 1 },
+                new ProductSize() { Id=3, Name = "M", Price = 100000, ProductId = 1 },
+                new ProductSize() { Id=4, Name = "S", Price = 200000, ProductId = 2 },
+                new ProductSize() { Id=5, Name = "L", Price = 200000, ProductId = 2 },
+                new ProductSize() { Id=6, Name = "M", Price = 200000, ProductId = 2 },
             };
                 context.ProductSizes.AddRange(productSizes);
                 context.SaveChanges();
